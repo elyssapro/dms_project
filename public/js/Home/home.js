1db8f97c3f894b9f011f2991d80b8f1eaf31b2e4
elyssapro <147242220+elyssapro@users.noreply.github.com>
2023-11-01 11:21:27 +0200

    finished making school result page now the next part is to make the skeleton

--- public/js/Home/home.js
+++ public/js/Home/home.js
@@ -1,3 +1,17 @@
+window.onscroll = function() {myFunction()};
+        
+    var navbar = document.querySelector(".header-section");
+    var sticky = navbar.offsetTop;
+    
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+        } else {
+        navbar.classList.remove("sticky");
+        }
+    }
+
+
 function openSearchPanel(){
     document.querySelector('.search-panel').style.display = 'block';
     var search_input = document.querySelector('.search-input');
