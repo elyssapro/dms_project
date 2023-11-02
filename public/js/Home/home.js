e33d2a34168ada4a9cb165e599c3bc0e0dc47556
elyssapro <147242220+elyssapro@users.noreply.github.com>
2023-11-02 12:33:37 +0200

    changed the swiper in the mission section and removed the cube effect from the swiper slide

--- public/js/Home/home.js
+++ public/js/Home/home.js
@@ -1,15 +1,15 @@
 window.onscroll = function() {myFunction()};
         
-    var navbar = document.querySelector(".header-section");
-    var sticky = navbar.offsetTop;
-    
-    function myFunction() {
-        if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-        } else {
-        navbar.classList.remove("sticky");
-        }
+var navbar = document.querySelector(".header-section");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    } else {
+    navbar.classList.remove("sticky");
     }
+}
 
 
 function openSearchPanel(){
@@ -18,71 +18,47 @@ function openSearchPanel(){
     search_input.focus();
 }
 
-  const progressCircle = document.querySelector(".autoplay-progress svg");
-  const progressContent = document.querySelector(".autoplay-progress span");
-  var swiper = new Swiper(".mySwiperautoplay", {
+const inte_grid = document.querySelector('.inte-grid');
+const inteTemplate = document.getElementById('small-content-container');
+for(let i = 0; i < 4; i++){
+  inte_grid.append(inteTemplate.content.cloneNode(true));
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(res => res.json())
+.then(posts => {
+  inte_grid.innerHTML = '';
+  posts.forEach(post => {
+    const div = inteTemplate.content.cloneNode(true);
+    div.querySelector('[data-content-title]').textContent = post.title;
+    div.querySelector('[data-content-body]').textContent = post.body;
+    inte_grid.append(div)
+  })
+})
+
+
+var swiperslidesystem = new Swiper(".mySwiperslidesystem", {
+    slidesPerView: 3,
     spaceBetween: 30,
-    centeredSlides: true,
     autoplay: {
-      delay: 2500,
-      disableOnInteraction: false
+    delay: 3000,
+    disableOnInteraction: false,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    on: {
-      autoplayTimeLeft(s, time, progress) {
-        progressCircle.style.setProperty("--progress", 1 - progress);
-        progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-      }
+});
+
+
+var swipervertical = new Swiper(".mySwipervertical", {
+  direction: "vertical",
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
     }
-  });
+});
 
-
-  var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-      },
-      pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      },
-      navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-      },
-  });
-
-  var swiper_cube = new Swiper(".mySwipercube", {
-      effect: "cube",
-      grabCursor: true,
-      autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-      },
-      cubeEffect: {
-      shadow: true,
-      slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94,
-      },
-      pagination: {
-      el: ".swiper-pagination",
-      },
-  });
 
   const testmony_grid = document.querySelector('.test-grid');
   const testTemplate = document.getElementById('test-template');
-  for(let i = 0; i < 4; i++) {
+  for(let i = 0; i < 8; i++) {
     
     testmony_grid.append(testTemplate.content.cloneNode(true));
   }
@@ -99,39 +75,4 @@ function openSearchPanel(){
     })
   })
 
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-  var swiper_cube = new Swiper(".mySwipercube", {
-    effect: "cube",
-    grabCursor: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    cubeEffect: {
-      shadow: true,
-      slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
   
