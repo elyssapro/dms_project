948e7b8ae539d7c276b7e594b627bac5540cad05
elyssapro <147242220+elyssapro@users.noreply.github.com>
2023-11-07 18:07:14 +0200

    changed the design for the search-results

--- public/js/Home/SearchResults/results.js
+++ public/js/Home/SearchResults/results.js
@@ -25,17 +25,3 @@ fetch('https://jsonplaceholder.typicode.com/posts')
         grid.append(div);
     })
 })
-
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("header-section");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
